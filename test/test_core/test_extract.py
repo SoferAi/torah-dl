25d39a76fc37d4702be4fb346e8ea3ebc6cebeb6
Matanya <matanya@sofer.ai>
2025-05-27 17:16:12 -0600

    fix: flakey tests

--- test/test_core/test_extract.py
+++ test/test_core/test_extract.py
@@ -5,6 +5,7 @@ from torah_dl import extract
 from torah_dl.core.exceptions import ExtractorNotFoundError
 
 
+@pytest.mark.flaky(reruns=3)
 @pytest.mark.parametrize("extractor, url, download_url, title, file_format, valid", get_all_the_tests(only_valid=True))
 def test_extract(extractor, url: str, download_url: str, title: str, file_format: str, valid: bool):
     extraction = extract(url)
--- test/test_core/test_extractors.py
+++ test/test_core/test_extractors.py
@@ -5,11 +5,13 @@ from torah_dl.core.exceptions import TorahDLError
 from torah_dl.core.models import Extractor
 
 
+@pytest.mark.flaky(reruns=3)
 @pytest.mark.parametrize("extractor, url, download_url, title, file_format, valid", get_all_the_tests())
 def test_can_handle(extractor: Extractor, url: str, download_url: str, title: str, file_format: str, valid: bool):
     assert extractor.can_handle(url)
 
 
+@pytest.mark.flaky(reruns=3)
 @pytest.mark.parametrize("extractor, url, download_url, title, file_format, valid", get_all_the_tests())
 def test_extract(extractor: Extractor, url: str, download_url: str, title: str, file_format: str, valid: bool):
     if not valid:
